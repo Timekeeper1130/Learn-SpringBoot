ddd0f23b10be2271b5368853bb1244554fba26a6
Timekeeper <632561223@qq.com>
2022-11-09 18:46:11 +0800

    修改

--- src/main/java/com/timekeeper/learnspringboot/StudySpringBootApplication.java
+++ src/main/java/com/timekeeper/learnspringboot/StudySpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSpringBootApplication {
+public class StudySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnSpringBootApplication.class, args);
+        SpringApplication.run(StudySpringBootApplication.class, args);
     }
 
 }
